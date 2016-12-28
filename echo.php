06cf4f2744a366da1c8afff547cacaff4be1422a
shawsola <yang_791949970@sina.com>
2016-12-28 18:50:06 +0800

    change echo

--- echo.php
+++ echo.php
@@ -7,7 +7,7 @@ $timestamp = $_GET["timestamp"];
 $nonce = $_GET["nonce"];
 $echoStr = $_GET["echostr"];
 
-$tmpArr = array($this->token, $timestamp, $nonce);
+$tmpArr = array(TOKEN, $timestamp, $nonce);
 sort($tmpArr);
 $tmpStr = implode($tmpArr);
 $tmpStr = sha1($tmpStr);
